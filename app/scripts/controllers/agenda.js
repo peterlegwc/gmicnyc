4f70d1f30464a511e4229c71e90bf6a0bda5ebb4
Peter <peter.le@gwc.net>
2015-04-07 17:19:49 -0700

    Agenda typo fixes, started speaker popup framework

--- app/scripts/controllers/agenda.js
+++ app/scripts/controllers/agenda.js
@@ -32,8 +32,7 @@ angular.module('gmicnycApp')
         'title': 'Brands and Customer Engagement',
         'startTime': '2015-06-11 10:00:00',
         'endTime': '2015-06-11 10:50:00',
-        'description': 'The close bond mobile users have with their devices gives brands the chance to resonate emotionally with customers. But just getting your message heard among the thousands vying for consumer’s attention can be a huge challenge. In this session, you’ll get the inside scoop from brands that have figured out how to rise above the noise by creating groundbreaking customer experiences.',
-        'topic': 'Using Mobile to Deepen Customer Engagement',
+        'description': '',
         'moderator': '',
         'speakers': [
           {
@@ -73,7 +72,7 @@ angular.module('gmicnycApp')
         'title': 'All About Content? Pieces to the Contextualization Puzzle',
         'startTime': '2015-06-11 11:00:00',
         'endTime': '2015-06-11 11:50:00',
-        'description': 'Creating a truly personalized customer experience that seamlessly spans digital and brick-and-mortar is now possible. The journey might start on desktop, but an evolving and aspirational message winding its way through a customer’s day has tremendous guiding influence. Learn how to move beyond a multichannel strategy by mastering an omnichannel approach that elevates your customer experience to new heights.',
+        'description': '',
         'topic': 'Contextualization, Omni-channel, User Experience, Personlization, Multi-channel',
         'moderator': {
           'firstname': 'Yev',
@@ -121,7 +120,7 @@ angular.module('gmicnycApp')
         'title': 'Lunch',
         'startTime': '2015-06-11 12:30:00',
         'endTime': '2015-06-11 13:15:00',
-        'description': 'Lunch',
+        'description': 'Lunch Break',
         'topic': 'Break',
         'moderator': '',
         'speakers': '',
@@ -139,7 +138,7 @@ angular.module('gmicnycApp')
         'title': 'Making Beacons Work for You',
         'startTime': '2015-06-11 13:50:00',
         'endTime': '2015-06-11 14:40:00',
-        'description': 'Beacon triggered marketing is a relatively new but highly effective way to deliver the right message to the right customer at exactly the right time. Targeting moving customers with location technology promises to boost retail sales but also adds a layer of complexity to customer relationships. Join this panel session led by early adopters to discover potential ways beacons can help you monetize your mobile audience, increase engagement with mobile users and enhance the shopping experience.',
+        'description': '',
         'topic': 'Beacons, Real Time Retail',
         'moderator': {
           'firstname': 'Dan',
@@ -186,11 +185,10 @@ angular.module('gmicnycApp')
         'speakers': '',
         'format': 'Break'
       }, {
-        'title': 'Marketing to Millenials: The Holy Grail',
+        'title': 'Marketing to Millennials: The Holy Grail',
         'startTime': '2015-06-11 15:30:00',
         'endTime': '2015-06-11 16:20:00',
-        'description': 'This is truly the first generation to grow up completely online and as a result, the marketing mix used to target them needs to evolve. Millennials want to interact with brands, to co-create products and to participate in the brand experience. How do you best serve an autonomous, responsive, tech savvy population? These 80 million U.S. customers will soon have the most spending power and engaging them on mobile is a must.',
-        'topic': 'Millennials: Participating in the Brand Experience',
+        'description': '',
         'moderator': '',
         'speakers': [
           {
@@ -207,7 +205,7 @@ angular.module('gmicnycApp')
         'title': 'Sand Hill Road to Wall Street: VC Perspectives',
         'startTime': '2015-06-11 16:25:00',
         'endTime': '2015-06-11 17:15:00',
-        'description': 'Investors from Sand Hill Road to Wall Street are making more big bets on billion dollars ideas than ever before. In this session, investors square off on what makes a great startup, a great exit and a great public company. To be sure, creating an unbeatable customer experience is the key ingredient to success.',
+        'description': '',
         'topic': 'VCs Perspectives on What Makes a Company Great',
         'moderator': '',
         'speakers': '',
--- app/scripts/controllers/speakers.js
+++ app/scripts/controllers/speakers.js
@@ -11,6 +11,7 @@ angular.module('gmicnycApp')
   .controller('SpeakersCtrl', function ($scope) {
     $scope.speakers = [
       {
+        'id': 1,
         'firstname': 'Ryan',
         'lastname': 'Matzner',
         'company': 'Fueled',
@@ -18,6 +19,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/ryan-matzner.jpg',
         'bio': 'Ryan Matzner is the Director of Strategy at Fueled, a mobile design and development shop based in NYC and London. He has a decade of online and mobile marketing experience, working with clients such as American Express, Condé Nast, Ideeli, Thrillist, HBO, QuizUp, Barneys, and Gilt Groupe. He can be found on Facebook, Twitter, LinkedIn and Google+.'
       },{
+        'id': 2,
         'firstname': 'Tyler',
         'lastname': 'McKinley',
         'company': 'MobileRQ',
@@ -25,6 +27,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/tyler-mckinley.jpg',
         'bio': ''
       },{
+        'id': 3,
         'firstname': 'David',
         'lastname': 'Berkowitz',
         'company': 'MRY',
@@ -32,6 +35,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/david-berkowitz.jpg',
         'bio': 'David Berkowitz is Chief Marketing Officer at agency MRY, a division of Publicis Groupe, where he directs the agency\'s communications and gains visibility for clients such as Visa, Johnson & Johnson, and Coca-Cola. Previously, he spent seven years at 360i, serving as Vice President of Emerging Media; he co-founded its social media practice and led its Startup Outlook initiative. David has contributed more than 500 columns to outlets such as Ad Age, MediaPost, and Mashable, and he has penned his blog MarketersStudio.com since 2005. He has spoken at more than 250 events globally, including a commencement address at his alma mater, Binghamton University.'
       },{
+        'id': 4,
         'firstname': 'Jeff',
         'lastname': 'Arbour',
         'company': 'Plyfe',
@@ -39,6 +43,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/jeff-arbour.jpg',
         'bio': ''
       },{
+        'id': 5,
         'firstname': 'Fabio',
         'lastname': 'Sisinni',
         'company': 'Shopkick',
@@ -46,6 +51,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/fabio-sisinni.jpg',
         'bio': ''
       },{
+        'id': 6,
         'firstname': 'Teal',
         'lastname': 'Newland',
         'company': 'StumbleUpon',
@@ -53,6 +59,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/teal-newland.jpg',
         'bio': ''
       },{
+        'id': 7,
         'firstname': 'Steve',
         'lastname': 'Callanan',
         'company': 'Wirewax',
@@ -60,6 +67,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/steve-callanan.jpg',
         'bio': 'Steve, an Electronic Engineering major, established his first production company in 2001 producing movie trailers, motion graphics, music videos and commercials; and was also producing over 400 hours of branded, short-form fashion and beauty content for the four biggest lifestyle publishers. He proposed that online video content should be as \'connected\' as the rest of the web; linking pixels to people, products and information was the goal. He engineered the world\'s first shoppable video player which lead to the most powerful interactive video platform to help hundreds of global brands realize their video potential.'
       },{
+        'id': 8,
         'firstname': 'Jill',
         'lastname': 'Wilson',
         'company': 'SGN',
@@ -67,6 +75,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/jill-wilson.jpg',
         'bio': ''
       },{
+        'id': 9,
         'firstname': 'Dan',
         'lastname': 'Herman',
         'company': 'ChatID',
@@ -74,6 +83,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/dan-herman.jpg',
         'bio': ''
       },{
+        'id': 10,
         'firstname': 'Wesley',
         'lastname': 'Barrow',
         'company': 'Nomi',
@@ -81,6 +91,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/wesley-barrow.jpg',
         'bio': ''
       },{
+        'id': 11,
         'firstname': 'Ryan',
         'lastname': 'Craver',
         'company': 'Lamour Group',
@@ -88,6 +99,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/ryan-craver.jpg',
         'bio': 'Ryan\'s natural fascination with the intersection of retail and technology has led to a career of inspiring and leading retailers to be on the forefront of the evolving "connected retail" environment. Ryan enjoys tackling changing consumer dynamics and defining how retail organizations and brands can leverage technology to strengthen their competitive position. Ryan currently leads Emerging Brands, Licensing and Digital Strategy for Lamour Group\'s portfolio of global brands while advising several startups focused on Mobile and AdTech. Prior to joining Lamour, Ryan was in charge of the core department store strategy at Lord & Taylor / Hudson\'s Bay Company and was a senior leader at Accenture, driving growth and profit improvement across multiple clients, including Virgin Media, FedEx and Best Buy.'
       },{
+        'id': 12,
         'firstname': 'Yev',
         'lastname': 'Galper',
         'company': 'EPAM',
@@ -95,6 +107,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/yev-galper.jpg',
         'bio': ''
       },{
+        'id': 13,
         'firstname': 'Nick',
         'lastname': 'Rockwell',
         'company': 'Condé Nast',
@@ -102,6 +115,7 @@ angular.module('gmicnycApp')
         'picture': '/images/speakers/nick-rockwell.jpg',
         'bio': ''
       },{
+        'id': 14,
         'firstname': 'Alex',
         'lastname': 'Wang',
         'company': 'Baidu',
@@ -110,4 +124,8 @@ angular.module('gmicnycApp')
         'bio': ''
       }
     ];
+    $scope.currentSpeaker = -1;
+    $scope.setDialog = function() {
+      $scope.currentSpeaker = this.speaker.id;
+    };
   });
